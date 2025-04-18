773ac4b9dbddd6b46413f381073564cde7489d13
SKYNEET1 <snrout18@gmail.com>
2025-04-18 19:24:51 +0530

    first commit

--- midleware/app.js
+++ midleware/app.js
@@ -2,17 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const studentAuthRouter = require('../routes/studentAuth');
+const dotenv = require('dotenv')
+dotenv.config();
+
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect("mongodb://127.0.0.1:27017/studentDB")
+mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log("DB Error:", err));
 
 app.use('/', studentAuthRouter);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Server running on port 3000");
 });
