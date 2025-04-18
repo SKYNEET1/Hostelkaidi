575a5fa4b9158bb9b9ea45c262eeeb25bf7980f8
SKYNEET1 <snrout18@gmail.com>
2025-04-18 19:47:43 +0530

    first commit

--- midleware/app.js
+++ midleware/app.js
@@ -4,13 +4,16 @@ const cookieParser = require('cookie-parser');
 const studentAuthRouter = require('../routes/studentAuth');
 const dotenv = require('dotenv')
 dotenv.config();
+console.log(dotenv.config())
 
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL,{
+    serverSelectionTimeoutMS: 30000
+})
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log("DB Error:", err));
 
