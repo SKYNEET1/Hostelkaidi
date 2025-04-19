72fcfd69359863b08694c2380b5f14bbb47c8521
SKYNEET1 <snrout18@gmail.com>
2025-04-19 06:25:10 +0530

    first commit

--- routes/studentAuth.js
+++ routes/studentAuth.js
@@ -128,15 +128,6 @@ router.put('/:uniqid/update-return', async (req, res) => {
     }
 });
 
-const express = require('express');
-const bcrypt = require('bcrypt');
-const Student = require('../model/Schema');
-const router = express.Router();
-
-const generateUniqueID = () => {
-    return Math.floor(10000 + Math.random() * 90000).toString(); // 5-digit random number
-};
-
 // ✅ SIGNUP ROUTE
 router.post('/signup', async (req, res) => {
     try {
